d68609375a78cabc64086db2e79d514e5c5d0b84
isa1090396 <73727389+isa1090396@users.noreply.github.com>
2020-12-05 11:44:43 +0800

    Add files via upload

---
+++ 16.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 19 11:15:38 2020
+
+@author: carols1107
+"""
+
+import random
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+
+
+x,y,z = mc.player.getTilePos()
+for i in range(20):
+    d = random.randrange(1,5)
+    
+    if d == 1:
+        mc.setBlocks(x,y,z,x+4,y,z,57)
+        x = x+4
+    if d == 2:
+        mc.setBlocks(x,y,z,x-4,y,z,57)
+        x = x-4
+    if d == 3:
+        mc.setBlocks(x,y+6,z,x,y,z,57)
+        y = y+4
+    if d == 4:    
+        mc.setBlocks(x,y+4,z,x,y,z,57)
+        y = y-4
+    
+    
+  
+  
+    
+        
+       
+        
+        
+      
+        
+        
+        
+    
+
+
+        
+       
+                 
---
+++ 17.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 19 11:15:38 2020
+
+@author: carols1107
+"""
+
+from time import sleep
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+while True:
+    mc.executeCommand("time add 100")
+    sleep(0.05)
+
+
+
+        
+        
+        
+    
+
+
+        
+       
+                 
